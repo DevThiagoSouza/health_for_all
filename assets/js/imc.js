251e5f7e0d826675bda4a36b35dc8619602a0d17
Gabriel-Cara-LMSE <gabriel.cara.lmse@gmail.com>
2023-10-25 15:27:32 -0300

    finale

--- assets/js/imc.js
+++ assets/js/imc.js
@@ -1,8 +1,32 @@
 
 
+const acimaPeso = document.querySelector(".alert-danger");
+const normal = document.querySelector(".alert-success");
+const abaixoPeso = document.querySelector(".alert-warning");
+
 function calcularIMC() {
-    var peso = document.getElementById('peso').value;
-    var altura = document.getElementById('altura').value / 100;
-    var imc = peso / (altura * altura);
+    let peso = document.getElementById('peso').value;
+    let altura = document.getElementById('altura').value / 100;
+    let imc = parseInt(peso / (altura * altura));
     document.getElementById('resultado').value = imc.toFixed(2);
-}
+
+        if (imc < 18) {
+            abaixoPeso.classList.remove("d-none");
+            setTimeout(function(){ abaixoPeso.classList.add("d-none"); }, 2000);
+        } else if (imc >= 18 && imc < 25) {
+            normal.classList.remove("d-none");
+            setTimeout(function(){ normal.classList.add("d-none"); }, 2000);
+        } else {
+            acimaPeso.classList.remove("d-none");
+            setTimeout(function(){ acimaPeso.classList.add("d-none"); }, 2000);
+        }
+
+    } 
+
+
+
+    
+
+
+
+
